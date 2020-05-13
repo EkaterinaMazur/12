70a71d6e2a0cbe7cf9cf6cc2d81f14240f3dee05
Екатерина Мазур <ekaterinamazur@MacBook-Pro-Ekaterina.local>
2020-05-14 00:47:16 +0700

    first deploy

---
+++ client/components/profile.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link, useParams  } from 'react-router-dom'
+
+
+const Profile = () => {
+  const { user }= useParams()
+  return (
+    <div>
+      <div id="title">
+        Profile
+      </div>
+      <div id="username">
+        {user}
+      </div>
+      <Link to='/dashboard'>Go To Root </Link>
+      <Link to='/dashboard/main'>Go To Main</Link>
+    </div>
+  )
+}
+
+Profile.propTypes = {}
+
+export default Profile
